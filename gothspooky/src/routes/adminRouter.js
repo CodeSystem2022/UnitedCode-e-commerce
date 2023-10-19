cbf9f84dac736de0d01118ca64f8e4312b0141a1
Rodrigo Garayalde Borges <rodrii.gborges@gmail.com>
2023-10-19 19:11:19 -0300

    Se empieza el CRUD con JSON

--- gothspooky/src/routes/adminRouter.js
+++ gothspooky/src/routes/adminRouter.js
@@ -1,9 +1,15 @@
 const router = require('express').Router();
-const { admin } = require('../controllers/adminController');
+const { admin, vistaCrear, crear } = require('../controllers/adminController');
+const multerProduct = require('../middlewares/multerProduct');
 
 
 /* GET users listing. */
 router.get('/', admin);
 
+///////// creacion de productos //////////
+////Formulario de creacion - vista///////
+router.get('/crear', vistaCrear);
+///Crear producto///
+router.post('/', multerProduct.array('imagenes'), crear);
 
 module.exports = router;
