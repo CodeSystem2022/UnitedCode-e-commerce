56f1c82486363c7f741fa30c50e5a6a80d32ad40
cabreraangelgabriel@gmail.com <62511940+cabreraangelgabriel@users.noreply.github.com>
2023-11-08 00:39:05 -0300

    login se conecta a la bd
    se conecta a la base para iniciar sesion de un usuario, verificando si existe o no.

--- gothspooky/src/controllers/usersController.js
+++ gothspooky/src/controllers/usersController.js
@@ -33,6 +33,31 @@ const controller = {
     }, 
     login:(req, res) =>{
         
+        const { email, contraseña } = req.body;
+        
+        // Buscar el usuario en la base de datos por el correo electrónico
+        db.Usuario.findOne({ where: { email: email } })
+            .then(usuario => {
+                if (!usuario) {
+                    
+                    // El correo electrónico no se encontró en la base de datos
+                    res.send("Usuario no encontrado. Debe registrarse primero.");
+                } else {
+                    // Comparar la contraseña ingresada con la contraseña almacenada en la base de datos
+                    if (bcrypt.compareSync(contraseña, usuario.contraseña)) {
+                        // Contraseña válida, el usuario está autenticado
+                        //res.send("Inicio de sesión exitoso");
+                        res.redirect('/');
+                    } else {
+                        // Contraseña incorrecta
+                        res.send("Contraseña incorrecta");
+                    }
+                }
+            })
+            .catch(error => {
+                res.send("Error al iniciar sesión");
+                console.log(error);
+            });
     }
 };
 
