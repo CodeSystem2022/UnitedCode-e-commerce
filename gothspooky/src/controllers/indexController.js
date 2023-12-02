f29973242f55f42b6222f967e0b5a2dcbad5351e
Rodrigo Garayalde Borges <rodrii.gborges@gmail.com>
2023-12-02 01:08:02 -0300

    Home con productos dinamicos

--- gothspooky/src/controllers/indexController.js
+++ gothspooky/src/controllers/indexController.js
@@ -1,9 +1,42 @@
-//Aca vamos a hacer el pedido de productos a la base de datos
-
+const db = require('../database/models');
 
 module.exports = {
     home: (req, res) => {
-        res.render('index');
+        let productosOfertas = db.Producto.findAll({
+            include: [
+                { model: db.Imagen,
+                    as: "imagen" 
+                },
+                {
+                    model: db.Categoria,
+                    as: "categoria",
+                    where: {
+                        nombre: "Ofertas"
+                    }
+                }
+            ]
+        });
+        let productosIngresantes = db.Producto.findAll({
+            include: [
+                { model: db.Imagen,
+                    as: "imagen" 
+                },
+                {
+                    model: db.Categoria,
+                    as: "categoria",
+                    where: {
+                        nombre: "Nuevos"
+                    }
+                }
+            ]
+        });
+        Promise.all([productosOfertas, productosIngresantes])
+        .then(([ofertas, nuevos]) => {
+            res.render("index", { ofertas, nuevos });
+        })
+        .catch((error) => {
+            console.log(error);
+        });
     }
 
 }
