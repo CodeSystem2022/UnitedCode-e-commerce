c676ceb1a7592e1df01d2ad140349a731f560c2d
waltermoya <wmmoya@gmail.com>
2023-12-07 19:08:06 -0300

    act

--- gothspooky/public/js/cart.js
+++ gothspooky/public/js/cart.js
@@ -1,46 +1,82 @@
 const vistaProductos = document.getElementById("vistaProductos");
-const preciototal = document.getElementById("total");
-let total = 0;
+// const preciototal = document.getElementById("total");
+// let total = 0;
+// const vistageneral = document.getElementById("probando")
 
-productos.forEach((product) => {
-  const content = document.createElement("div");
-  content.innerHTML = `
-    <div class="product">
-        <div class="tarjeta">
-            <img class="product-img" src="${product.img}">
-            <button class="remove-product">Eliminar</button>
-        </div>
-        <div class="product-info">      
-            <h4>${product.productName}</h4>
-        </div>
-        <div class="price">
-        <h4> $ ${product.price} </h4>
-        </div>
-    </div>
-    <hr>
+function tarjetaCarrito() {
+  const prendas = JSON.parse(localStorage.getItem("carrito"));
+  console.log(prendas);
+  prendas.forEach((element) => {
+    const nuevaPrenda = document.createElement("div");
+    nuevaPrenda.classList = "prenda-carrito";
+    nuevaPrenda.innerHTML = `
+      <h4>${element.title}</h4>
+      <h4>$${element.price}</h4>
     `;
-  vistaProductos.append(content);
-
-  const removeButton = content.querySelector(".remove-product");
-  removeButton.addEventListener("click", () => {
-    content.remove();
-    total -= product.price;
-    totalElementos.innerHTML = `<h3>Total: $ ${total}</h3>`;
-
-    if (total === 0) {
-      vistaProductos.innerHTML = "<h3>Tu carrito se encuentra vacío</h3>";
-    }
+    vistaProductos.append(nuevaPrenda);
   });
-
-  total += product.price;
-});
-
-const totalElementos = document.createElement("div");
-totalElementos.innerHTML = `<h3>Total: $ ${total}</h3>`;
-preciototal.append(totalElementos);
-
-if (total === 0) {
-  vistaProductos.innerHTML = `<h3>Tu carrito se encuentra vacío</h3>`;
 }
 
-cartBtn.addEventListener("click", "/");
+tarjetaCarrito();
+
+// productos.forEach((product) => {
+//   console.log(product)
+//   const prendas = JSON.parse(localStorage.getItem("carrito"));
+//   console.log(prendas)
+//   const content = document.createElement("div");
+//   content.innerHTML = `
+//     <div class="product">
+//         <div class="tarjeta">
+//             <img class="product-img" src="${product.img}">
+//             <button class="remove-product">Eliminar</button>
+//         </div>
+//         <div class="product-info">
+//             <h4>${product.productName}</h4>
+//         </div>
+//         <div class="price">
+//         <h4> $ ${product.price} </h4>
+//         </div>
+//     </div>
+//     <hr>
+//     `;
+//   vistaProductos.append(content);
+
+//   const removeButton = content.querySelector(".remove-product");
+//   removeButton.addEventListener("click", () => {
+//     content.remove();
+//     total -= product.price;
+//     totalElementos.innerHTML = `<h3>Total: $ ${total}</h3>`;
+
+//     if (total === 0) {
+//       vistaProductos.innerHTML = "<h3>Tu carrito se encuentra vacío</h3>";
+//     }
+//   });
+
+//   total += product.price;
+// });
+
+// const totalElementos = document.createElement("div");
+// totalElementos.innerHTML = `<h3>Total: $ ${total}</h3>`;
+// preciototal.append(totalElementos);
+
+// if (total === 0) {
+//   vistaProductos.innerHTML = `<h3>Tu carrito se encuentra vacío</h3>`;
+// }
+
+// carrito.forEach((products) => {
+//   const content = document.createElement("div");
+
+//   content.innerHTML = `
+//   <div>
+//       <div class="product-info">
+//           <h4>${products.title}</h4>
+//       </div>
+//       <div class="price">
+//       <h4> $ ${products.price} </h4>
+//       </div>
+//   </div>
+//   <hr>
+//   `;
+//   vistageneral.append(content);
+//   console.log(JSON.parse(localStorage.getItem("carrito")))
+// });
--- gothspooky/public/js/inicio.js
+++ gothspooky/public/js/inicio.js
@@ -1,6 +1,7 @@
- 
-
-
+const botonComprar = document.querySelectorAll(".buttonroducto-detalles");
+const carrito = [];
+const vistageneral = document.getElementById("probando")
+console.log(botonComprar);
 
 //Carrusel
 let sliderInner = document.querySelector(".slider--inner");
@@ -8,11 +9,51 @@ let images = sliderInner.querySelectorAll("img");
 let index = 1;
 
 setInterval(function () {
-    let percentage = index *-101;
-    sliderInner.style.transform = "translateX(" + percentage + "%)";
-    index++
-    if (index >= (images.length -1)){
-        index = 0;
-    }
+  let percentage = index * -101;
+  sliderInner.style.transform = "translateX(" + percentage + "%)";
+  index++;
+  if (index >= images.length - 1) {
+    index = 0;
+  }
+}, 2000);
+
+botonComprar.forEach((boton) => {
+  boton.addEventListener("click", (e) => {
+    const producto = e.target.parentElement;
+    //console.log(e.target.parentElement);
+    vistageneral.innerHTML = ""
+    carrito.push({
+      quantity: 1,
+      title: producto.querySelector("h3").textContent,
+      price: producto.querySelector("p").textContent,
+      imagen: producto.querySelector("img"),
+    });
+    console.log(carrito);
     
-},2000)
+    carrito.forEach((products) => {
+    const content = document.createElement("div");
+    
+    content.innerHTML = `
+      <div>
+          <div class="product-info">      
+              <h4>${products.title}</h4>
+          </div>
+          <div class="price">
+          <h4> $ ${products.price} </h4>
+          </div>
+      </div>
+      <hr>
+      `;
+    vistageneral.append(content);
+    saveLocal();
+})
+  });
+
+  const saveLocal = () => {
+    localStorage.setItem("carrito",JSON.stringify(carrito))
+  };
+  
+  
+});
+
+
