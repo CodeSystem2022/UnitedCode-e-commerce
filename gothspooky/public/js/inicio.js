46fb66f9deb9bc765084691ca225fb154ef708bf
Walter Moya <wmmoya@gmail.com>
2023-12-20 09:08:52 -0300

    actualizo

--- gothspooky/public/js/inicio.js
+++ gothspooky/public/js/inicio.js
@@ -1,15 +1,6 @@
-<<<<<<< Updated upstream
-const botonComprar = document.querySelectorAll(".buttonroducto-detalles");
-const carrito = [];
-const vistageneral = document.getElementById("probando")
+const botonComprar = document.querySelectorAll(".producto-agregar");
 console.log(botonComprar);
-=======
-const botonComprar = document.querySelectorAll(".buttonproducto-detalles");
 const carrito = [];
-const vistageneral = document.getElementById("probando")
-console.log(botonComprar);
-
->>>>>>> Stashed changes
 
 //Carrusel
 let sliderInner = document.querySelector(".slider--inner");
@@ -25,87 +16,42 @@ setInterval(function () {
   }
 }, 2000);
 
-botonComprar.forEach((boton) => {
-  boton.addEventListener("click", (e) => {
-    const producto = e.target.parentElement;
-    //console.log(e.target.parentElement);
-    vistageneral.innerHTML = ""
-    carrito.push({
-      quantity: 1,
-      title: producto.querySelector("h3").textContent,
-      price: producto.querySelector("p").textContent,
-      imagen: producto.querySelector("img"),
-    });
-    console.log(carrito);
-    
-<<<<<<< Updated upstream
-    carrito.forEach((products) => {
-    const content = document.createElement("div");
-    
-    content.innerHTML = `
-      <div>
-          <div class="product-info">      
-              <h4>${products.title}</h4>
-          </div>
-          <div class="price">
-          <h4> $ ${products.price} </h4>
-          </div>
-      </div>
-      <hr>
-      `;
-    vistageneral.append(content);
-    saveLocal();
-})
-  });
-
-  const saveLocal = () => {
-    localStorage.setItem("carrito",JSON.stringify(carrito))
-  };
-  
-  
-});
-
-
-=======
-},2000)
-
-botonComprar.forEach((boton) => {
+const agregarPedido = () => {
+  botonComprar.forEach((boton) => {
     boton.addEventListener("click", (e) => {
       const producto = e.target.parentElement;
-      //console.log(e.target.parentElement);
-      vistageneral.innerHTML = ""
       carrito.push({
         quantity: 1,
-        title: producto.querySelector("h3").textContent,
+        title: producto.querySelector("h4").textContent,
         price: producto.querySelector("p").textContent,
-        imagen: producto.querySelector("img"),
+        imagen: producto.querySelector("img").src,
       });
       console.log(carrito);
-      
       carrito.forEach((products) => {
-      const content = document.createElement("div");
-      
-      content.innerHTML = `
-        <div>
-        <img> $ ${products.imagen} </img>
-            <div class="product-info">      
-                <h4>${products.title}</h4>
-            </div>
-            <div class="price">
-                <h4> $ ${products.price} </h4>
-            </div>
-        </div>
-        <hr>
-        `;
-      vistageneral.append(content);
-      saveLocal();
-  })
+        saveLocal();
+        contarCarrito();
+      });
     });
-  
+
     const saveLocal = () => {
-      localStorage.setItem("carrito",JSON.stringify(carrito))
+      localStorage.setItem("carrito", JSON.stringify(carrito));
     };
-    
-    
   });
->>>>>>> Stashed changes
+};
+
+const contarCarrito = () => {
+  const count = document.querySelector("#cart-Counter");
+  const memoria = JSON.parse(localStorage.getItem("carrito"));
+  if (memoria && memoria.length > 0) {
+    const cuenta = memoria.reduce(
+      (acum, current) => acum + current.quantity,
+      0
+    );
+    count.innerText = cuenta;
+  } else {
+    count.innerText = 0;
+  }
+};
+
+agregarPedido();
+contarCarrito();
